85ba0ca432cfbe93b2b7537c04ac937836d495cc
Dieter Gobbers <dieter@gobbers.de>
2016-06-06 09:29:23 +0200

    fixed the variables names
    fixed the variables names due to the renamed plugin

--- inc/languages/english/admin/groupsort.lang.php
+++ inc/languages/english/admin/groupsort.lang.php
@@ -1,13 +1,13 @@
 <?php
 // english
 
-$l['grouporder_title'] = 'Group Order';
+$l['groupsort_title'] = 'Group Order';
 
-$l['grouporder_rank_description'] = 'sort each users groups by display order';
-$l['grouporder_rank_description'] = 'lower number = higher priority, the highest priority group a users is a member of will be his primary and display group.<br>'.
+$l['groupsort_rank_description'] = 'sort each users groups by display order';
+$l['groupsort_rank_description'] = 'lower number = higher priority, the highest priority group a users is a member of will be his primary and display group.<br>'.
                                                                         'This is the same as the "Order" column you can see on the "Groups" page but for all groups, not just the ones marked as "show on forum team page".';
 
-$l['grouporder_task_description'] = 'Update the sequence of each users groups by the groups display order';
+$l['groupsort_task_description'] = 'Update the sequence of each users groups by the groups display order';
 
 // english
 ?>
