1070906925c3201b68c45dc9bab7562d6a2262bd
Dieter Gobbers <dieter@gobbers.de>
2016-06-08 13:46:14 +0200

    Create groupsort.php
    added missing file

---
+++ inc/tasks/groupsort.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+Group Order Plugin for MyBB
+Copyright (C) 2013 Dieter Gobbers
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+        die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+function task_grouporder($task)
+{
+        grouporder_reorder_all_users( );
+
+        add_task_log($task, "reordered all groups of all users");
+}
+
+?>
